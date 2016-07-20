fce50150df55baad23d12be37dd0a48c0d94a19a
nmor4 <nick-morin@hotmail.com>
2016-07-20 13:38:35 -0400

    Initial commit

---
+++ Assignment 5/Assignment 5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Author: Nick Morin
+ * Description: Assignment 5 - File I/O and Exception Handling
+ * Last Modified:
+ */
+
+
+namespace Assignment_5
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
